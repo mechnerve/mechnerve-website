b987539dade3839790d8edffd147d2fa75ad63f8
mechnerve <mechnervesolutions@gmail.com>
2026-01-23 15:05:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,5 +14,9 @@ def about():
 def contact():
     return render_template("contact.html")
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
