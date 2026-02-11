f12cfdd762d124a1635dce09245264758b14a393
mechnerve <mechnervesolutions@gmail.com>
2026-02-11 21:34:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -251,7 +251,10 @@ def health():
 # RUN
 # ==================================================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+
 
 
 
